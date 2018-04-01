e7e123fd76e0734b7ede5a61146a0f0344256863
Max Mumford <maxmumford@gmail.com>
2018-04-02 00:15:19 +0100

    Update readme and naming

--- src/YoutubeFilterExtension.php
+++ src/YoutubeFilterExtension.php
@@ -1,15 +1,15 @@
 <?php
 
-namespace Bolt\Extension\MaxMumford\YoutubeFilter;
+namespace Bolt\Extension\MaxMumford\YouTubeFilter;
 
 use Bolt\Extension\SimpleExtension;
 
 /**
- * YoutubeFilter extension class.
+ * YouTubeFilter extension class.
  *
  * @author Max Mumford <maxmumford@gmail.com>
  */
-class YoutubeFilterExtension extends SimpleExtension
+class YouTubeFilterExtension extends SimpleExtension
 {
   protected function registerTwigFilters() {
     require_once __DIR__ . '/Filters.php';
