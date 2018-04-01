3126e65b457a0f71e8185a2b188f62a9a5a5156d
Max Mumford <maxmumford@gmail.com>
2018-04-02 00:11:22 +0100

    Implement filter

--- src/Filters.php
+++ src/Filters.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * Regex credit: https://stackoverflow.com/a/6382259/939543
+ */
 function youtubeFilterFunc($url) {
-  return $url . "SUFFIXTEST";
+  if (preg_match('%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i', $url, $match)) {
+    return "https://www.youtube.com/embed/" . $match[1];
+  }
+  return $url;
 }
